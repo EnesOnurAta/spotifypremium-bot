c11ed46ff21278c6ad39c3b7e1b3f4a4119c29e7
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-08 07:57:11 +0300

    v3
    Değişen oynuyor eklendi
    Bir kaç tane özellik ekledik

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 const ayarlar = require('./ayarlar.json');
 const bot = new Discord.Client();
 var prefix = ayarlar.prefix;
+message.prefix = prefix;
+
 
 //Komutlar Klasöründeki .js uzantılı dosyaları komut olarak algılaması için
 client.commands = new Discord.Collection();
@@ -71,4 +73,28 @@ client.unload = command => {
   });
 };
 
+//Değişen Oynuyor Kısmı
+bot.on('ready', () => {
+	console.log("Yukleniyor...");
+	setTimeout(function(){
+	console.log("Spotify Premium hesap dagitmaya devam ediyor");
+	}, 1000);
+	function botStatus() {
+        let status = [
+            `Prefix 》${botconfig.prefix}`,
+            `Teşekkürler 》${bot.guilds.size} sunucu.`,
+            `Bedava Spotify Premium Hesapları.`,
+            `Botun Geliştiricisi 》 Enes Onur Ata#9427`,
+            `Teşekkürler 》${bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} kullanıcı.`
+        ];
+        let rstatus = Math.floor(Math.random() * status.length);
+
+        bot.user.setActivity(status[rstatus], {Type: 'STREAMING'});        // BOT STATUS
+      }; setInterval(botStatus, 20000)
+        setInterval(() => {
+        dbl.postStats(bot.guilds.size)
+        }, 1800000);
+	})
+
+//Token
 client.login(process.env.BOT_TOKEN);
